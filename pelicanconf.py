8032c2b69769e52f392b4cda1645d7d125598dc5
Kousuke Ebihara <kousuke@co3k.org>
2013-12-22 21:26:07 +0900

    some tuned to follow current co3k.org url layout

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,26 +6,26 @@ AUTHOR = u'Kousuke Ebihara'
 SITENAME = u'co3k.org'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Tokyo'
 
 DEFAULT_LANG = u'ja'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
+
+THEME = "themes/basic"
+MENUITEMS = [("About", "/"), ("Blog", "/category/blog.html")]
+ARTICLE_SAVE_AS = "blog/{slug}.html"
+ARTICLE_URL = "blog/{slug}.html"
+ARTICLE_LANG_URL = "blog/{slug}-{lang}.html"
+ARTICLE_LANG_SAVE_AS = "blog/{slug}-{lang}.html"
+AUTHOR_SAVE_AS = ""
+AUTHOR_URL = ""
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
