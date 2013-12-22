283d2b2f7cd3d60a8b3380859ea82e1201c597bc
Kousuke Ebihara <kousuke@co3k.org>
2013-12-22 18:06:32 +0900

    changed publishment configuration for feeds and preview

--- publishconf.py
+++ publishconf.py
@@ -10,11 +10,11 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://co3k.org/'
-RELATIVE_URLS = False
+SITEURL = 'http://co3k.org'
+RELATIVE_URLS = True
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = 'feeds/blog.atom'
+CATEGORY_FEED_ATOM = 'feeds/category-%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
